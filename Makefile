e13943abee8c1c4840fd756d0ed029b3637b698e
Luis Fernando Gomes <dev@luiscoms.com.br>
2017-02-18 21:54:07 -0200

    Update nodejs versions (#3)
    * Update nodejs versions
    
    * Update image streams
    
    * Fix nss_wrapper permission
    
    * Changes to run on Mac OS
    
    * Fix remaining versions
    
    * Fix squash image
    
    * Remove squash logic
    
    * Remove versions from Dockerfile

--- Makefile
+++ Makefile
@@ -2,7 +2,7 @@
 BASE_IMAGE_NAME=s2i-nodejs
 ONBUILD_IMAGE_NAME=nodejs
 NAMESPACE=ryanj
-VERSIONS = 0.10.46 0.12.15 4.4.7 5.12.0 6.3.1
+VERSIONS = 0.10.48 0.12.18 4.7.3 5.12.0 6.9.5 7.5.0
 
 # Include common Makefile code.
 include hack/common.mk
--- hack/build.sh
+++ hack/build.sh
@@ -1,5 +1,5 @@
 #!/bin/bash -e
-# This script is used to build, test and squash the OpenShift Docker images.
+# This script is used to build and test the OpenShift Docker images.
 #
 # Name of resulting image will be: 'NAMESPACE/OS-BASE_IMAGE_NAME:NODE_VERSION'.
 #
@@ -13,7 +13,7 @@ OS=${1-$OS}
 VERSION=${2-$VERSION}
 
 DOCKERFILE="Dockerfile"
-if [[ -v ONBUILD ]]; then
+if [[ ! -z "$ONBUILD" ]]; then
   BASE_IMAGE_NAME="${ONBUILD_IMAGE_NAME}"
   DOCKERFILE+=".onbuild"
 fi
@@ -25,23 +25,10 @@ trap "rm -f ${DOCKERFILE}.${version}" SIGINT SIGQUIT EXIT
 function docker_build_with_version {
   cp ${DOCKERFILE} "${DOCKERFILE}.${version}"
   git_version=$(git rev-parse HEAD)
-  sed -i "${DOCKERFILE}.${version}" -e "s/NODE_VERSION *= *.*/NODE_VERSION=${version} \\\/"
+  sed -i.bak -e "s/NODE_VERSION *= *.*/NODE_VERSION=${version} \\\/" "${DOCKERFILE}.${version}"
   echo "LABEL io.origin.builder-version=\"${git_version}\"" >> "${DOCKERFILE}.${version}"
   docker build -t ${IMAGE_NAME}:${version} -f "${DOCKERFILE}.${version}" .
-  if [[ "${SKIP_SQUASH}" != "1" ]]; then
-    squash "${DOCKERFILE}.${version}"
-  fi
-  rm -f "${DOCKERFILE}.${version}"
-}
-
-# Install the docker squashing tool[1] and squash the result image
-# [1] https://github.com/goldmann/docker-squash
-function squash {
-  # FIXME: We have to use the exact versions here to avoid Docker client
-  #        compatibility issues
-  easy_install -q --user docker_py==1.6.0 docker-squash==1.0.0rc6
-  base=$(awk '/^FROM/{print $2}' $1)
-  ${HOME}/.local/bin/docker-squash -f $base ${IMAGE_NAME}:${version}
+  rm -f "${DOCKERFILE}.${version}" "${DOCKERFILE}.${version}.bak"
 }
 
 # Specify a VERSION variable to build a specific nodejs.org release
@@ -51,7 +38,7 @@ versions=${VERSION:-$VERSIONS}
 for version in ${versions}; do
   IMAGE_NAME="${NAMESPACE}/${OS}-${BASE_IMAGE_NAME}"
 
-  if [[ -v TEST_MODE ]]; then
+  if [[ ! -z "$TEST_MODE" ]]; then
     IMAGE_NAME+="-candidate"
   fi
 
@@ -64,7 +51,7 @@ for version in ${versions}; do
     docker_build_with_version Dockerfile
   fi
 
-  if [[ -v TEST_MODE ]]; then
+  if [[ ! -z "$TEST_MODE" ]]; then
     IMAGE_NAME=${IMAGE_NAME} NODE_VERSION=${version} test/run
 
     if [[ $? -eq 0 ]] && [[ "${TAG_ON_SUCCESS}" == "true" ]]; then
